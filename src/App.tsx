932df49305411b9d7b36a38c73ee3855b0e9da85
MurAd <muradisayevazer@gmail.com>
2023-06-28 12:21:09 +0400

    total donations query connected

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,22 @@ import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
-  Code,
   Grid,
   Heading,
   extendTheme
 } from '@chakra-ui/react';
 import { Logo } from './Logo';
 import { Counter } from './donation/Counter';
+import { useQuery } from 'urql';
+
+const TotalDonationsQuery = `
+  query Query {
+    totalDonations
+  }
+`;
+
+
 
 const theme = extendTheme({
   fonts: {
@@ -24,25 +31,34 @@ const theme = extendTheme({
   }
 });
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3} bg="gray.50">
-        <VStack spacing={8}>
-          <Logo h="32" pointerEvents="none" />
-          <Heading as="h1" size="xl">
-            JOIN THE MOVEMENT!
-          </Heading>
-          <Text>
-            The team is growing everyday and scoring wins for the planet.
-            <br /> Remove trash with us and track our progress!
-          </Text>
+export const App = () => {
+  const [{ data, fetching, error }] = useQuery({
+    query: TotalDonationsQuery
+  });
 
-          <Heading as="h2" size="4xl">
-            <Counter from={0} to={23423423} />
-          </Heading>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-);
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3} bg="gray.50">
+          <VStack spacing={8}>
+            <Logo h="32" pointerEvents="none" />
+            <Heading as="h1" size="xl">
+              JOIN THE MOVEMENT!
+            </Heading>
+            <Text>
+              The team is growing everyday and scoring wins for the planet.
+              <br /> Remove trash with us and track our progress!
+            </Text>
+
+            <Heading as="h2" size="4xl">
+              <Counter from={0} to={data.totalDonations} />
+            </Heading>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+};
