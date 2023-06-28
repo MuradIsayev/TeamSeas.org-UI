2c1042be4ae12ac6cf9e1d648a47419a409f9a19
MurAd <muradisayevazer@gmail.com>
2023-06-28 22:57:45 +0400

    mutation for submit added

--- src/donation/DonationWizard.tsx
+++ src/donation/DonationWizard.tsx
@@ -3,20 +3,43 @@ import { Box, VStack } from '@chakra-ui/layout';
 import React, { useState } from 'react';
 import { CountSelection } from './CountSelection';
 import { DonationDetails } from './DonationDetails';
+import { useMutation } from 'urql';
+
+const CreateDonation = `
+    mutation Mutation($createDonationInput: CreateDonationInput!) {
+        createDonation(createDonationInput: $createDonationInput) {
+            id
+            count
+            createdAt
+        }
+    }
+`;
 
 interface Props {}
 
 export const DonationWizard = (props: Props) => {
   const [step, setStep] = useState(0);
   const [donationDetails, setDonationDetails] = useState({ count: 20 });
+  const [donationResult, createDonation] = useMutation(CreateDonation);
 
   const next = (values: any = {}) => {
     const mergedDetails = { ...donationDetails, ...values };
-    setStep(step + 1);
 
-    setDonationDetails(mergedDetails);
+    if (step === pages.length - 1) {
+      submitDonation(mergedDetails);
+    } else {
+      setStep(step + 1);
+      setDonationDetails(mergedDetails);
+    }
   };
+
   const previous = () => setStep(step - 1);
+
+  const submitDonation = async (values: any) => {
+    await createDonation({
+      createDonationInput: values
+    });
+  };
 
   const pages = [
     <CountSelection next={next} initialCount={donationDetails.count} />,
