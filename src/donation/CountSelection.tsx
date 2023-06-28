4605ed822c83c52ca2c79d4cad2426ab4faf5c96
MurAd <muradisayevazer@gmail.com>
2023-06-28 18:00:31 +0400

    heading and text added

--- src/donation/CountSelection.tsx
+++ src/donation/CountSelection.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { option } from 'yargs';
 import RadioCard from './RadioCard';
 import { useRadioGroup } from '@chakra-ui/radio';
-import { SimpleGrid, VStack } from '@chakra-ui/layout';
+import { Heading, SimpleGrid, Text, VStack } from '@chakra-ui/layout';
 import { NumberInput, NumberInputField } from '@chakra-ui/number-input';
 import { Button } from '@chakra-ui/button';
 
@@ -36,6 +36,12 @@ export const CountSelection = ({ initialCount, next }: Props) => {
 
   return (
     <VStack spacing={4} align="stretch">
+      <Heading as="h2" size="md">
+        JOIN #TEAMSEAS
+      </Heading>
+      <Text fontSize="md" fontWeight="bold">
+        $1 removes a pound of trash
+      </Text>
       <SimpleGrid mt={1} columns={2} spacing={2} {...group}>
         {options.map(value => {
           const radio = getRadioProps({ value, enterKeyHint: '' });
