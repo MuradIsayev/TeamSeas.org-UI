af2e3f6c791e73f6c7220a29c3dc8aa2ddc3479c
MurAd <muradisayevazer@gmail.com>
2023-06-28 17:21:10 +0400

    page pagination mock added

--- src/donation/DonationWizard.tsx
+++ src/donation/DonationWizard.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import { Button } from '@chakra-ui/button';
+import { Box, VStack } from '@chakra-ui/layout';
+import React, { useState } from 'react';
 
 interface Props {}
 
 export const DonationWizard = (props: Props) => {
-  return <div>DonationWizard</div>;
+  const [step, setStep] = useState(0);
+
+  const next = () => setStep(step + 1);
+  const previous = () => setStep(step - 1);
+
+  const pages = [<div>Step 1</div>, <div>Step 2</div>];
+
+  return (
+    <Box boxShadow="xl" p={8} bg="white" borderRadius="xl" minW="sm">
+      {pages[step]}
+
+      <VStack spacing={2}>
+        <Button
+          w="full"
+          colorScheme="blue"
+          size="lg"
+          borderRadius="full"
+          onClick={next}
+        >
+          Next
+        </Button>
+        <Button
+          w="full"
+          onClick={previous}
+          size="lg"
+          borderRadius="full"
+          variant="ghost"
+          fontSize="sm"
+          color="gray.700
+            "
+        >
+          Previous
+        </Button>
+      </VStack>
+    </Box>
+  );
 };
